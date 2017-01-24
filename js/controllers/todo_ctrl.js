d91d5b54c3cb98aea1cd5322790df9c645f2e909
Pramod Jacob <psjacob68@gmail.com>
2017-01-24 12:01:39 -0500

    working on CRUD actions & filtering

--- js/controllers/todo_ctrl.js
+++ js/controllers/todo_ctrl.js
@@ -1,7 +1,5 @@
-ToDo.controller('ToDoCtrl', ['$scope',
-  function($scope) {
-    $scope.item =
-
+ToDo.controller('ToDoCtrl', ['$scope', '$window', '$filter',
+  function($scope, $window, $filter) {
     $scope.items = [{
       text: 'Get grocieries from the store',
       dueDate: new Date(),
@@ -16,5 +14,21 @@ ToDo.controller('ToDoCtrl', ['$scope',
       completed: false
     }];
 
+    $scope.formData = {};
+    $scope.formData.dueDate = new Date();
+
+    $scope.addToDo = function(newToDo) {
+      $scope.items.push({
+        text: $scope.formData.text,
+        dueDate: $scope.formData.dueDate,
+        completed: false
+      })
+      $scope.formData.text = "";
+      newToDo.$setPristine();
+    }
+
+    $scope.removeToDo = function($index) {
+      $scope.items.splice($index, 1); 
+    }
   }
 ]);
